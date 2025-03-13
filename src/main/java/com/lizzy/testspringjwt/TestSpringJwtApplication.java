e10aef5b8861c4c4c7270a7e2ca6395570e7fcb0
gebeen-dev <rrdd1207@gmail.com>
2025-03-13 09:49:54 +0900

    Initial commit

---
+++ src/main/java/com/lizzy/testspringjwt/TestSpringJwtApplication.java
@@ -0,0 +1,13 @@
+package com.lizzy.testspringjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lizzy/testspringjwt/TestSpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizzy.testspringjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
